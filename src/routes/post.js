9a5c5a9f6aa136dce59bcc86a211a151a0d903dc
CSchink <45950072+CSchink@users.noreply.github.com>
2020-02-10 18:04:11 -0500

    new stuff

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,12 +1,30 @@
-var MongoClient = require('mongodb').MongoClient;
-var uri="mongodb+srv://dbCorey:MVDhmYhNQkp2y8T@cluster0-ymebw.mongodb.net/sottlab?retryWrites=true&w=majority"
+const {MongoClient} = require('mongodb');
 
-MongoClient.connect(uri, function(err, db) {
-  if (err) throw err;
-  var database = db.db("sottlab");
-  database.createCollection("Lighthouse", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-});
+async function main(){
+    
+    const uri="mongodb+srv://dbCorey:MVDhmYhNQkp2y8T@cluster0-ymebw.mongodb.net/sottlab?retryWrites=true&w=majority"
+   
+    const client = new MongoClient(uri);
+
+    try {
+        
+        await client.connect();
+
+   
+        await createListing(client,
+            {
+               name: "New sample"
+            }
+        );
+  
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function createListing(client, newListing){
+    const result = await client.db("sample").collection("sottlab").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
