799552405b25fef8d7641f617ebd3b5a0ed53b92
CSchink <45950072+CSchink@users.noreply.github.com>
2020-03-04 10:29:55 -0500

    post route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const connection = require('./connection.js')
 const express = require('express');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
     res.send('Hello John!!');
