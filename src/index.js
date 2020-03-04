77f4f0051a736a8f23a8676ac51fa093734f197a
CSchink <45950072+CSchink@users.noreply.github.com>
2020-03-04 10:32:50 -0500

    mongo connect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const connection = require('./connection.js')
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
