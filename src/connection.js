64c3d4c3eb69a5657153635dfff80dbbaf78c59f
CSchink <45950072+CSchink@users.noreply.github.com>
2020-08-15 09:52:22 -0400

    new stuff

--- src/connection.js
+++ src/connection.js
@@ -1,17 +1,16 @@
-async function connect(){
-    const uri="mongodb+srv://dbCorey:MVDhmYhNQkp2y8T@cluster0-ymebw.mongodb.net/sottlab?retryWrites=true&w=majority"
-    
+async function connect() {
+  const uri =
+    "mongodb+srv://dbCorey:MVDhmYhNQkp2y8T@cluster0-ymebw.mongodb.net/sottlab?retryWrites=true&w=majority";
 
-    const {MongoClient} = require('mongodb');
-    const client = new MongoClient(uri);
-    return await client.connect();
+  const { MongoClient } = require("mongodb");
+  const client = new MongoClient(uri);
+  return await client.connect();
 }
 
 // async function main(){
 //     const uri="mongodb+srv://dbCorey:MVDhmYhNQkp2y8T@cluster0-ymebw.mongodb.net/sottlab?retryWrites=true&w=majority"
 //     const {MongoClient} = require('mongodb');
 //     const client = new MongoClient(uri);
-  
 
 //     try {
 //     await client.connect();
@@ -33,67 +32,106 @@ async function connect(){
 // }
 // main().catch(console.error);
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    return databasesList.databases
-};
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
 
-async function listEntries(client){
-    const cursor = await client.db('sottlab').collection('historylab1').find({});
-    let results = await cursor.toArray();
-    return results;
+  return databasesList.databases;
 }
 
-
-async function userCheck(client, username, password){
-    const cursor = await client.db('sottlab').collection('logindata').find({}).limit(10);
-    let results = await cursor.toArray();
-    let confirmation = results.some(function(result) {
-        console.log(result)
-        if(result.user===username && result.password === password){
-            return true
-        } else {
-            return false
-        }
+async function listEntries(client) {
+  const cursor = await client.db("sottlab").collection("historylab1").find({});
+  let results = await cursor.toArray();
+  results.forEach((result) => {
+    Object.keys(result).forEach((key) => {
+      if (typeof result[key] === "string") {
+        result[key] = result[key].trim();
+      } else if (Array.isArray(result[key])) {
+        result[key] = result[key]
+          .filter((value) => value != null)
+          .map((value) => value.trim());
+      }
     });
-    return confirmation;
+  });
+
+  return results;
 }
 
-async function deleteEntries(client, userName){
-    result = await client.db("sottlab").collection("logindata").deleteOne({ user: userName })
-    console.log(`${result.deletedCount} document(s) were deleted.`)
+async function userCheck(client, username, password) {
+  const cursor = await client
+    .db("sottlab")
+    .collection("logindata")
+    .find({})
+    .limit(10);
+  let results = await cursor.toArray();
+  let confirmation = results.some(function (result) {
+    console.log(result);
+    if (result.user === username && result.password === password) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return confirmation;
 }
 
-async function createEntry(client, newEntry){
-    const result = await client.db("sottlab").collection("historylab1").insertOne(newEntry);
-    console.log(`New entry created with the following id: ${result.insertedId}`);
-    return result
+async function deleteEntries(client, userName) {
+  result = await client
+    .db("sottlab")
+    .collection("logindata")
+    .deleteOne({ user: userName });
+  console.log(`${result.deletedCount} document(s) were deleted.`);
 }
 
-async function createScienceEntry(client, newEntry){
-    const result = await client.db("sottlab").collection("sciencelab").insertOne(newEntry);
-    console.log(`New entry created with the following id: ${result.insertedId}`);
-    return result
+async function createEntry(client, newEntry) {
+  const result = await client
+    .db("sottlab")
+    .collection("historylab1")
+    .insertOne(newEntry);
+  console.log(`New entry created with the following id: ${result.insertedId}`);
+  return result;
 }
 
-async function userConfirm(){
-    
-    let userData = axios.get('http://localhost:3000/userCheck')
-    let results = userData.filter(x => {return x.user})
-    return results;
+async function createScienceEntry(client, newEntry) {
+  const result = await client
+    .db("sottlab")
+    .collection("sciencelab")
+    .insertOne(newEntry);
+  console.log(`New entry created with the following id: ${result.insertedId}`);
+  return result;
 }
 
-async function editData(client, result){
-    let editData = await client.db('sottlab').collection('historylab').find({'Entry': result})
-    let editResult = await editData.toArray();
-    return editResult;
+async function userConfirm() {
+  let userData = axios.get("http://localhost:3000/userCheck");
+  let results = userData.filter((x) => {
+    return x.user;
+  });
+  return results;
 }
 
-async function listScienceEntries(client){
-    const cursor = await client.db('sottlab').collection('sciencelab').find({});
-    let results = await cursor.toArray();
-    return results;
+async function editData(client, result) {
+  let editData = await client
+    .db("sottlab")
+    .collection("historylab")
+    .find({ Entry: result });
+  let editResult = await editData.toArray();
+  return editResult;
 }
 
-module.exports = { listScienceEntries, userCheck, createScienceEntry, userConfirm, listDatabases, connect, listEntries, deleteEntries, createEntry, editData }
+async function listScienceEntries(client) {
+  const cursor = await client.db("sottlab").collection("sciencelab").find({});
+  let results = await cursor.toArray();
+  return results;
+}
+
+module.exports = {
+  listScienceEntries,
+  userCheck,
+  createScienceEntry,
+  userConfirm,
+  listDatabases,
+  connect,
+  listEntries,
+  deleteEntries,
+  createEntry,
+  editData,
+};
