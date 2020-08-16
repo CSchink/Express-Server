612e59c0f8914d41641db31723ec6dfb91ac9b0d
CSchink <45950072+CSchink@users.noreply.github.com>
2020-08-16 08:54:37 -0400

    edit data

--- src/connection.js
+++ src/connection.js
@@ -111,11 +111,23 @@ async function userConfirm() {
 }
 
 async function editData(client, entry) {
-  let id = entry._Id;
+  let id = entry._id;
   let editData = await client
     .db("sottlab")
     .collection("historylab1")
-    .updateOne({ _Id: id });
+    .updateOne({ _id: id }, {
+        Date: entry.Date,
+        Entry: entry.Entry,
+        Century: entry.Century,
+        Category: entry.Category,
+        Origin: entry.Origin,
+        Target: entry.Target,
+        Cultural: entry.Cultural,
+        ptags: entry.ptags,
+        htags: entry.htags,
+        Source: entry.Source,
+        Page: entry.Page
+    });
   return editData
 }
 
