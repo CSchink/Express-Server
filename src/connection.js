110ebf19e54dd393fdafdac226f60504a4ba699a
CSchink <45950072+CSchink@users.noreply.github.com>
2020-08-17 09:43:44 -0400

    new stuff

--- src/connection.js
+++ src/connection.js
@@ -110,7 +110,8 @@ async function userConfirm() {
 async function editData(client, entry) {
   console.log(entry);
   const ObjectID = require('mongodb').ObjectID;
-  let query = {_id: new ObjectID(entry._id)}
+  let id = new ObjectID(entry._id)
+  let query = {_id: id}
   
   let update = {
       Date: entry.Date,
