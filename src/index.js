379a1d7af81093b34a863e6c9f0f4589c157edc7
CSchink <45950072+CSchink@users.noreply.github.com>
2020-08-17 12:43:43 -0400

    new stuff

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const secretKey = "Johnny Be Good";
 app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("port", PORT)
+
 // Express Router:
 // const router = express.Router();
 // router.get()
@@ -76,12 +76,6 @@ app.use((req, res, next) => {
   // }
 
   // next();
-});
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 app.get("/", function (req, res) {
