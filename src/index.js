7bc324339596c10176f15c5b343199cb152eff7a
CSchink <45950072+CSchink@users.noreply.github.com>
2020-08-31 12:34:51 -0400

    cors for pusher

--- src/index.js
+++ src/index.js
@@ -43,10 +43,10 @@ app.post("/login", async function (req, res, next) {
   }
 });
 
+
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  console.log("middleware");
   // verify token
 
   let token = req.headers.authorization;
